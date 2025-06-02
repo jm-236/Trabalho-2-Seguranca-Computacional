36e52cd3171e1298c825de0526971d31b1e041ff
jm-236 <jaowjjw@gmail.com>
2025-06-02 11:37:33 -0300

    Função S-nibbles adicionada.

--- s-aes.cpp
+++ s-aes.cpp
@@ -9,11 +9,54 @@ bitset<16> add_round_key(bitset<16> mensagem, bitset<16> chave) {
     return mensagem xor chave;
 }
 
+bitset<16> s_nibbles(bitset<16> mensagem) {
+    
+    cout << endl << "--------------S-Nibbles-------------" << endl;
+
+    vector<bitset<4>> s_box = { 
+                                0b1001, 0b0100, 0b1010, 0b1011,
+                                0b1101, 0b0001, 0b1000, 0b0101, 
+                                0b0110, 0b0010, 0b0000, 0b0011,
+                                0b1100, 0b1110, 0b1111, 0b0111 
+                            };
+
+    bitset<16> nova_mensagem;
+    bitset<4> nibble;
+    int cont = 1; // contador da nibble atual
+    
+    for(int i = 15; i >= 3; i -= 4){
+        
+        // pegando os bits de 4 em 4
+        nibble[3] = mensagem[i];
+        nibble[2] = mensagem[i - 1];
+        nibble[1] = mensagem[i - 2];
+        nibble[0] = mensagem[i - 3];
+
+        bitset<4> correspondente_s_box = s_box[nibble.to_ullong()];
+
+        cout << "Nibble " << cont << ": " << dec << nibble.to_ullong() << endl;
+        cout << "Correspondente na s-box: " << correspondente_s_box << endl << endl;
+        
+        // inserindo os bits correspondentes na nova mensagem
+        nova_mensagem[i] = correspondente_s_box[3];
+        nova_mensagem[i - 1] = correspondente_s_box[2];
+        nova_mensagem[i - 2] = correspondente_s_box[1];
+        nova_mensagem[i - 3] = correspondente_s_box[0];
+
+        cont++;
+    }
+
+    cout << "Mensagem após s-nibble: " << hex << nova_mensagem.to_ullong() << endl;
+
+    return nova_mensagem;
+}
+
 bitset<16> S_AES(bitset<16> mensagem, bitset<16> chave) {
     
     bitset<16> mensagem_apos_round_key = add_round_key(mensagem, chave);
     cout << "Mensagem após round key: " << hex << mensagem_apos_round_key.to_ullong() << endl;
 
+    bitset<16> mensagem_apos_s_nibble = s_nibbles(mensagem_apos_round_key);
     return 0;
 }
 
