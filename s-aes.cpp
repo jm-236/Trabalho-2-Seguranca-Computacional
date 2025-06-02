5fa9d00bc4609ddeca8326f3d65c3a65ef4f67db
jm-236 <jaowjjw@gmail.com>
2025-06-02 11:57:28 -0300

    Função sift-rows implementada

--- s-aes.cpp
+++ s-aes.cpp
@@ -6,6 +6,7 @@ bitset<16> add_round_key(bitset<16> mensagem, bitset<16> chave) {
     cout << endl << "--------------Add Round Key-------------" << endl;
     cout << "Mensagem no round key: " << hex << mensagem.to_ullong() << endl;
     cout << "Chave no round key: " << hex << chave.to_ullong() << endl;
+    cout << "Mensagem após round key: " << hex << (mensagem xor chave).to_ullong() << endl;
     return mensagem xor chave;
 }
 
@@ -23,7 +24,7 @@ bitset<16> s_nibbles(bitset<16> mensagem) {
     bitset<16> nova_mensagem;
     bitset<4> nibble;
     int cont = 1; // contador da nibble atual
-    
+
     for(int i = 15; i >= 3; i -= 4){
         
         // pegando os bits de 4 em 4
@@ -51,12 +52,37 @@ bitset<16> s_nibbles(bitset<16> mensagem) {
     return nova_mensagem;
 }
 
+bitset<16> shift_rows(bitset<16> mensagem) {
+
+    cout << endl << "--------------Shift-Rows-------------" << endl;
+    
+    bitset<16> nova_mensagem;
+
+    cout << "Trocando os dois elementos da segunda linha..." << endl;
+
+    // bits que não mudarão
+    for(int i = 15; i >= 12; i--){
+        // bits que não trocam
+        nova_mensagem[i] = mensagem[i];
+        nova_mensagem[i - 8] = mensagem[i - 8];
+        
+        // bits que trocam de posição(2° com 4°)
+        nova_mensagem[i - 4] = mensagem[i - 12];
+        nova_mensagem[i - 12] = mensagem[i - 4];
+    }
+
+    cout << "Mensagem após shift-rows: " << hex << nova_mensagem.to_ullong() << endl;
+    return 0;
+}
+
 bitset<16> S_AES(bitset<16> mensagem, bitset<16> chave) {
     
     bitset<16> mensagem_apos_round_key = add_round_key(mensagem, chave);
-    cout << "Mensagem após round key: " << hex << mensagem_apos_round_key.to_ullong() << endl;
+    
 
     bitset<16> mensagem_apos_s_nibble = s_nibbles(mensagem_apos_round_key);
+
+    bitset<16> mensagem_apos_shift_rows = shift_rows(mensagem_apos_s_nibble);
     return 0;
 }
 
